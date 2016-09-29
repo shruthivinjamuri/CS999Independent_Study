09b9dbc1da37474317ef721e69551824eea749bb
shruthivinjamuri <shruthi@uwm.edu>
2016-09-29 10:16:37 -0500

    Writing a run method

---
+++ src/Implementations/Run.java
@@ -0,0 +1,19 @@
+package Implementations;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node a = new Node("a");
+		Node b = new Node("b");
+		Node c = new Node("c");
+		Edge ab = new Edge(a, b, 1.0);
+		Edge bc = new Edge(b, c, 2.0);
+		Edge ac = new Edge(a, c, 3.0);
+		Graph graph1 = new Graph("Graph1", 5.0);
+		graph1.addEdge(from, to, cost)
+		
+		
+	}
+
+}
